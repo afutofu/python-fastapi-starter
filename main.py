5c72bd08f4c19a78ee1472f310a963b33d52a239
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-21 20:22:27 -0700

    Create get, getbyid, and create route for todos

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
 
 app = FastAPI()
 
@@ -6,3 +14,22 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# todosMock: Todo = [{}]
+
+
+@app.get("/todos")
+async def get_todos():
+    return {"message": "Get todos"}
+
+
+@app.get("/todos/{todo_id}")
+async def get_todo_by_id(todo_id: int):
+    return {"message": "Get todos by id", "id": todo_id}
+
+
+@app.post("/todos")
+async def create_todo(todo: Todo):
+
+    return todo
